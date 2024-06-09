4efb50555bf0e04fe289aa4fe6f734195acb79ea
waldrumpus <waldrumpus@gmail.com>
2024-06-09 09:03:48 +0200

    convert to library project

---
+++ Compatibility.cs
@@ -0,0 +1,6 @@
+// Purpose: Contains the IsExternalInit class to allow for C# 9.0's init-only properties to be used in C# 8.0.
+// see https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-9#init-only-setters
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit {}
+}
